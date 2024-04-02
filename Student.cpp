bf9e9721b83730e213711a1369e0bd5ec9bd5434
demo admin <no@at.such>
2024-04-01 20:36:28 -0500

    The 2024 Initial Walkthrough updates class files, renames files and cleans modern compiler warnings.

--- Student.cpp
+++ Student.cpp
@@ -1,10 +1,12 @@
 // Clinton Garwood
-// Created by ncc306 on 3/18/22.
+// Created 2022, Updated 2024
 // Example for Student Class
 
 #include <string>
 #include <iostream>
+#include <utility>
 #include "Student.h"
+
 using namespace std;
 
 // Student Constructor Given No Data or Person Object
@@ -14,7 +16,7 @@ Student::Student()
         set_preferred_name(establish_student_name());
         set_student_id(establish_student_id_number());
         set_grade_point_average(establish_student_gpa());
-        set_degree_pursued(establish_student_degee_pursuit());
+        set_degree_pursued(establish_student_degree_pursuit());
         Student_Person = Person(establish_year(),establish_loc(),establish_name());
     }
 
@@ -27,25 +29,25 @@ Student::Student(Person a_person)
     set_preferred_name(establish_student_name());
     set_student_id(establish_student_id_number());
     set_grade_point_average(establish_student_gpa());
-    set_degree_pursued(establish_student_degee_pursuit());
+    set_degree_pursued(establish_student_degree_pursuit());
     }
 
 // Student Constructor Given no Student Data, and data for a Person Object (but no person object)
 // This constructor must prompt for student fields -test
 Student::Student(int the_year, std::string the_location, std::string the_name)
-    : Student_Person(the_year,the_location,the_name)
+    : Student_Person(the_year, std::move(the_location), std::move(the_name))
     {
     set_preferred_name(establish_student_name());
     set_student_id(establish_student_id_number());
     set_grade_point_average(establish_student_gpa());
-    set_degree_pursued(establish_student_degee_pursuit());
+    set_degree_pursued(establish_student_degree_pursuit());
     }
 
 // Student Constructor Given data for a Student Object, but no data for Person or Person object
 // Student(std::string, int, float, std::string);
 Student::Student(std::string as_name, int as_id, float as_gpa, std::string as_degree)
-    :  preferred_name{as_name}, student_id{as_id},
-       grade_point_average{as_gpa}, degree_pursued{as_degree}
+    :  preferred_name{std::move(as_name)}, student_id{as_id},
+       grade_point_average{as_gpa}, degree_pursued{std::move(as_degree)}
     {
        Student_Person = Person(establish_year(),establish_loc(),establish_name());
     }
@@ -53,29 +55,27 @@ Student::Student(std::string as_name, int as_id, float as_gpa, std::string as_de
 // Student Constructor Given data for a Student Object, and Person data, but no Person Object
 // Student::Student(std::string, int, float, std::string, int, std::string, std::string){}
 Student::Student(std::string a_name, int an_id, float start_gpa, std::string the_degree, int this_date, std::string this_loc, std::string this_name)
-    :  preferred_name{a_name}, student_id{an_id},
-       grade_point_average{start_gpa}, degree_pursued{the_degree},
-       Student_Person(this_date,this_loc,this_name)
+    :  preferred_name{std::move(a_name)}, student_id{an_id},
+       grade_point_average{start_gpa}, degree_pursued{std::move(the_degree)},
+       Student_Person(this_date, std::move(this_loc), std::move(this_name))
     {}
 
 // Student Constructor Given data for a Student Object, and a Person object
 // Student::Student(std::string, int, float, std::string, Person) {}
 Student::Student(std::string a_name, int an_id, float start_gpa, std::string the_degree, Person aPerson)
-    : preferred_name{a_name}, student_id{an_id},
-      grade_point_average{start_gpa}, degree_pursued{the_degree},
+    : preferred_name{std::move(a_name)}, student_id{an_id},
+      grade_point_average{start_gpa}, degree_pursued{std::move(the_degree)},
       Student_Person(aPerson.get_person_year(),aPerson.get_person_location(),aPerson.get_person_name())
     {}
-
 
 // Getters
 std::string Student::get_preferred_name() {
     return this->preferred_name;
 }
-
-int Student::get_student_id(){
+int Student::get_student_id() const{
     return this->student_id;
 }
-float Student::get_grade_point_average(){
+float Student::get_grade_point_average() const{
     return this->grade_point_average;
 }
 std::string Student::get_degree_pursued(){
@@ -85,23 +85,19 @@ std::string Student::get_degree_pursued(){
 std::string Student::get_student_p_name() {
     return Student_Person.get_person_name();
 }
-
 std::string Student::get_student_p_location() {
     return Student_Person.get_person_location();
 }
-
 int Student::get_student_p_year(){
     return Student_Person.get_person_year();
 };
-
 // test this report
 void Student::get_student_person_data(){
     Student_Person.report_person_data();
 }
-
 // Setters
 void Student::set_preferred_name(std::string this_name){
-    this->preferred_name = this_name;
+    this->preferred_name = std::move(this_name);
 }
 void Student::set_student_id(int std_id){
     this->student_id = std_id;
@@ -109,55 +105,45 @@ void Student::set_student_id(int std_id){
 void Student::set_grade_point_average(float new_gpa){
     this->grade_point_average = new_gpa;
 }
-
 void Student::set_degree_pursued(std::string pursuing_degree){
-    this->degree_pursued = pursuing_degree;
+    this->degree_pursued = std::move(pursuing_degree);
 }
-
-
 std::string Student::establish_student_name(){
     cout << "Please enter preferred name for student: " << endl;
-    std::string pname;
-    cin >> pname;
-    return pname;
+    std::string pref_name;
+    cin >> pref_name;
+    return pref_name;
 }
-
 int Student::establish_student_id_number(){
     cout << "Please enter student ID number: " << endl;
     int id_num;
     cin >> id_num;
     return id_num;
 }
-
 float Student::establish_student_gpa(){
     cout << "Please enter initial student GPA: " << endl;
     float gpa;
     cin >> gpa;
     return gpa;
 }
-
-std::string Student::establish_student_degee_pursuit(){
+std::string Student::establish_student_degree_pursuit(){
     cout << "Please enter degree being pursued: " << endl;
     std::string degree;
     cin >> degree;
     return degree;
 }
-
-
 int Student::establish_year(){
     cout << "Please enter the birth year: " << endl;
     int year;
     cin >> year;
     return year;
 }
-
 std::string Student::establish_loc(){
     cout << "Please enter the home city: " << endl;
     std::string city;
     cin >> city;
     return city;
 }
-
 std::string Student::establish_name(){
     cout << "Please enter given name: " << endl;
     std::string given_name;
