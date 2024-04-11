6e2f86d465f667931294d41c3007aaace81bf742
khairosclerosis <larentree470@gmail.com>
2024-04-11 14:51:00 -0500

    moved to challenge folder

--- docs/challenge/kaila_kylah_challenge_folder/FriendClass.cpp
+++ docs/challenge/kaila_kylah_challenge_folder/FriendClass.cpp
@@ -2,4 +2,12 @@
 // Created by laren on 4/11/2024.
 //
 
+#include <iostream>
 #include "FriendClass.h"
+#include "PrimaryClass.h" // include primaryclass.h to access primary class
+
+// Definition of showA() member function of class B
+void Friend::showPrimary(PrimaryClass obj) {
+    // Since B is a friend of A, it can access A's private members
+    std::cout << "Primary Class's private member: " << obj.privateMember << std::endl;
+}
