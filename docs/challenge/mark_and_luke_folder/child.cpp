1499b30e33ce1e7b4412dc5b98a05843698a29a7
Ravenswood19 <79956164+Ravenswood19@users.noreply.github.com>
2024-04-11 14:41:32 -0500

    Update child.cpp

--- docs/challenge/mark_and_luke_folder/child.cpp
+++ docs/challenge/mark_and_luke_folder/child.cpp
@@ -1,40 +1,5 @@
-/*
-  File: Rectangle.hpp
-  Author: Mark & Luke
-  Date: 4/11/24
-  Description: Header file for the Rectangle class, a child class of Shape.
-                 Header file for the Circle class, a child class of Shape.
-*/
+#include <iostream>
+#include "child.h"
 
-#ifndef RECTANGLE_HPP
-#define RECTANGLE_HPP
-
-#include "Shape.hpp"
-
-class Rectangle : public Shape {
-private:
-    double width;
-    double height;
-public:
-    Rectangle(double w, double h) : width(w), height(h) {}
-    double area() const override { return width * height; }
-    double perimeter() const override { return 2 * (width + height); }
-};
-
-#endif // RECTANGLE_HPP
-
-#ifndef CIRCLE_HPP
-#define CIRCLE_HPP
-
-#include "Shape.hpp"
-
-class Circle : public Shape {
-private:
-    double radius;
-public:
-    Circle(double r) : radius(r) {}
-    double area() const override { return 3.14159 * radius * radius; }
-    double perimeter() const override { return 2 * 3.14159 * radius; }
-};
-
-#endif // CIRCLE_HPP
+// Empty implementation for virtual functions
+// Already implemented in child.h
