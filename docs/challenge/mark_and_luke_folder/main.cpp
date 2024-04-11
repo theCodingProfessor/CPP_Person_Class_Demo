a54daa40e3f8c272fa58ba52f2c2c8c4248af41d
mfbavaro <159957534+mfbavaro@users.noreply.github.com>
2024-04-11 14:11:23 -0500

    Update main.cpp

--- docs/challenge/mark_and_luke_folder/main.cpp
+++ docs/challenge/mark_and_luke_folder/main.cpp
@@ -1,1 +1,22 @@
+#include "Shape.hpp"
+#include "Rectangle.hpp"
+#include "Circle.hpp"
+#include <iostream>
+
+int main() {
+    Rectangle rectangle(4, 5);
+    Circle circle(3);
+
+    // Polymorphic behavior
+    Shape* shapePtr1 = &rectangle;
+    Shape* shapePtr2 = &circle;
+
+    std::cout << "Rectangle Area: " << shapePtr1->area() << std::endl;
+    std::cout << "Rectangle Perimeter: " << shapePtr1->perimeter() << std::endl;
+
+    std::cout << "Circle Area: " << shapePtr2->area() << std::endl;
+    std::cout << "Circle Perimeter: " << shapePtr2->perimeter() << std::endl;
+
+    return 0;
+}
 
