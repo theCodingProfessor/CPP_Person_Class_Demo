67e28c8c426a9af99dd8f801e2ba4fe84a7542c6
Ravenswood19 <79956164+Ravenswood19@users.noreply.github.com>
2024-04-11 14:41:56 -0500

    Update parent.cpp

--- docs/challenge/mark_and_luke_folder/parent.cpp
+++ docs/challenge/mark_and_luke_folder/parent.cpp
@@ -1,20 +1,5 @@
-/*
-  File: Shape.hpp
-  Author: Luke & Mark
-  Date: 4/11/24
+#include <iostream>
+#include "parent.h"
 
-  Description: Header file for the Shape class, an abstract base class for geometric shapes.
-*/
-
-#ifndef SHAPE_HPP
-#define SHAPE_HPP
-
-class Shape {
-public:
-    virtual double area() const = 0; // Pure virtual function for calculating area
-    virtual double perimeter() const = 0; // Pure virtual function for calculating perimeter
-    virtual ~Shape() {} // Virtual destructor for proper cleanup
-};
-
-#endif // SHAPE_HPP
-
+// Empty implementation for virtual functions
+// Each child class will override these functions
