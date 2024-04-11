0c54c6e1cb8d0fdd628cbb2a7e63acc16ee7222d
mfbavaro <159957534+mfbavaro@users.noreply.github.com>
2024-04-11 14:41:24 -0500

    Update main.cpp

--- docs/challenge/mark_and_luke_folder/main.cpp
+++ docs/challenge/mark_and_luke_folder/main.cpp
@@ -1,11 +1,12 @@
 /**
  * File: main.cpp
- * Author: [Author Name]
- * Date: [Date]
+ * Author: Luke & Mark
+ * Date: 4/11/2024
  * Description: This file contains the main function demonstrating the usage
  * of the Shape class and its derived class Rectangle. It creates objects of
  * the Rectangle class, demonstrates polymorphic behavior by using pointers
- * to Shape, and displays information about the rectangles.
+ * to Shape, and displays information about the rectangles. 
+ * ChatGPT was used for assistance in parts of this assignment.
  */
 
 #include <iostream>
