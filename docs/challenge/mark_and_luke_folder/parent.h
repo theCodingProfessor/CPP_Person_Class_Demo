6dcf9662c954d41f664cf3f9f20d39cb3ab87b1f
Ravenswood19 <79956164+Ravenswood19@users.noreply.github.com>
2024-04-11 14:38:00 -0500

    Update parent.h

--- docs/challenge/mark_and_luke_folder/parent.h
+++ docs/challenge/mark_and_luke_folder/parent.h
@@ -1,1 +1,36 @@
+/**
+ * File: parent.h
+ * Author: [Author Name]
+ * Date: [Date]
+ * Description: This header file defines the parent class Shape, which represents
+ * geometric shapes. It contains pure virtual functions for calculating the area
+ * and perimeter of a shape, as well as a virtual function for displaying shape
+ * information.
+ */
+
+#ifndef PARENT_H
+#define PARENT_H
+
+// Parent class representing geometric shapes
+class Shape {
+public:
+    /**
+     * @brief Virtual function for calculating the area of the shape.
+     * @return The area of the shape.
+     */
+    virtual double area() const = 0;
+
+    /**
+     * @brief Virtual function for calculating the perimeter of the shape.
+     * @return The perimeter of the shape.
+     */
+    virtual double perimeter() const = 0;
+
+    /**
+     * @brief Virtual function for displaying shape information.
+     */
+    virtual void display() const = 0;
+};
+
+#endif // PARENT_H
 
