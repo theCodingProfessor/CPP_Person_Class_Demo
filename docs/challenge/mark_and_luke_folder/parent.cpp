16d054a154d8b4772fac2760e23a4a3ea6213188
mfbavaro <159957534+mfbavaro@users.noreply.github.com>
2024-04-11 14:17:09 -0500

    Update parent.cpp

--- docs/challenge/mark_and_luke_folder/parent.cpp
+++ docs/challenge/mark_and_luke_folder/parent.cpp
@@ -1,1 +1,12 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+class Shape {
+public:
+    virtual double area() const = 0; // Pure virtual function
+    virtual double perimeter() const = 0; // Pure virtual function
+    virtual ~Shape() {} // Virtual destructor
+};
+
+#endif // SHAPE_HPP
 
