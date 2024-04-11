6e4ad984280c238d854466ea10b2afdf907421c0
Angel <angelmontes7@yahoo.com>
2024-04-11 16:17:10 -0500

    Final Output

--- docs/challenge/angel_tony_challenge/main.cpp
+++ docs/challenge/angel_tony_challenge/main.cpp
@@ -1,23 +1,44 @@
-#include "Person.h"
-#include "IdClass.h"
-#include "IdClass.cpp"
-#include "Person.cpp"
+#include "Person.h"    // Include the definition of the Person class
+#include "IdClass.h"   // Include the definition of the IdClass class
+#include "Person.cpp"  // Include the implementation of the Person class
+#include "IdClass.cpp" // Include the implementation of the IdClass class
 
 int main() {
-    Person person1("Antonio", 1, "CSCE306"); // Assuming "first" and "third" are variables or enums defined elsewhere
+    // Create two Person objects
+    Person person1("Antonio", 1, "CSCE306");
     Person person2("Angel", 3, "CSCE306");
-    IdClass id(123456);
 
-    person1.displayId(id);
-    id.displayID(person1);
+    // Generate unique ID numbers
+    static int lastAssignedId = 123456; // Initialize the last assigned ID number
+    int idNumber1 = lastAssignedId++;   // Increment the last assigned ID number for person1
+    int idNumber2 = lastAssignedId++;   // Increment the last assigned ID number for person2
 
-    person2.displayId(id);
-    id.displayID(person2);
+    // Create IdClass objects with unique ID numbers
+    IdClass id1(idNumber1);
+    IdClass id2(idNumber2);
 
-     std::string name;
+    std::cout << "\n";
+    std::cout << "\n";
+    // Display information about person1
+    person1.displayInfo();
+    // Display the ID of person1
+    person1.displayId(id1);
+
+    std::cout << "\n";
+    std::cout << "\n";
+    
+    // Display information about person2
+    person2.displayInfo();
+    // Display the ID of person2
+    person2.displayId(id2);
+
+    std::cout << "\n";
+    std::cout << "\n";
+
+    // Variables to store user input
+    std::string name;
     int year;
     std::string course;
-    int idNumber;
 
     // Input for user
     std::cout << "Enter name: ";
@@ -26,7 +47,17 @@ int main() {
     std::cin >> year;
     std::cout << "Enter course: ";
     std::cin >> course;
+    std::cout << "\n";
+    std::cout << "\n";
+
+    // Increment last assigned ID number and use it for the new person
+    int idNumber3 = lastAssignedId++;
+    IdClass id3(idNumber3);
+    Person person3(name, year, course);
+    // Display information about the newly created person
+    person3.displayInfo();
+    // Display the ID of the newly created person
+    person3.displayId(id3);
 
     return 0;
 }
-
