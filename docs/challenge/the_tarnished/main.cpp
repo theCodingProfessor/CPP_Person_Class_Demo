32211e89f9a7c775b9f409a62eda39a168df3b1f
Santiago Ramirez <santirp04@gmail.com>
2024-04-11 14:37:22 -0500

    Update author names in main.cpp

--- docs/challenge/the_tarnished/main.cpp
+++ docs/challenge/the_tarnished/main.cpp
@@ -2,5 +2,5 @@
  * NCC Object-Oriented Programming
  * Spring 2024 C++ Lab Main
  * Assignment to 
- * @author: Bhavnoor Saini, Santiago Ramirez
+ * @author: Bhavnoor Saini (1354712), Santiago Ramirez (1346109)
  * */
