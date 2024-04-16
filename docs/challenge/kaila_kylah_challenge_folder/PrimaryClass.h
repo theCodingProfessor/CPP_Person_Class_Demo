d4d3db30a981157bdc5a08d88e4f71bebc3fffa8
KailaButt <160177608+KailaButt@users.noreply.github.com>
2024-04-16 14:45:42 -0500

    change

--- docs/challenge/kaila_kylah_challenge_folder/PrimaryClass.h
+++ docs/challenge/kaila_kylah_challenge_folder/PrimaryClass.h
@@ -6,14 +6,23 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+#include <string>
+
 class PayrollSystem; // Forward declaration
 
 class Employee {
     friend class PayrollSystem;
 
 public:
+    // Constructor
     Employee(const std::string& name, int employeeID, double salary);
 
+    // Getter functions for retrieving private member variables
+    std::string getName() const;
+    int getEmployeeID() const;
+    double getSalary() const;
+
+    // Setter function for modifying the 'salary' member variable
     void setSalary(double newSalary);
 
 private:
