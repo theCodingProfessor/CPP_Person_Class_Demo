a35262b35a65eb23fe4e1e4b86588f72eeba3f75
dylanoandersen <dylanoandersen@gmail.com>
2024-04-16 14:11:11 -0500

    done

---
+++ docs/challenge/KDcars/honda.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <utility>
+#include "honda.h"
+
+using namespace std;
+
+
+honda::honda(const Cars& car_object, const string engine) : Cars(car_object), engine(engine) {
+
+}
+
+// Setter definition
+void honda::set_engine_type(string engine) {
+    this->engine = engine;
+}
+
+// Getter definition
+string honda::get_engine_type() {
+    return engine;
+}
--- docs/challenge/KDcars/honda.h
+++ docs/challenge/KDcars/honda.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "cars.h"
 
 
@@ -8,14 +9,14 @@ using namespace std;
 #ifndef HONDA_H
 #define HONDA_H
 
-class honda : public cars{
+class honda : public Cars{
 public:
-    string get_model_name();
-    
+    honda(const Cars& car_object, const string engine);
+    void set_engine_type(string enigne);
+    string get_engine_type();
 
-
-
-
+private:
+    string engine;
 
 };
 
