289489a2ee95270c178caae13b2687229a181a66
Ravenswood19 <79956164+Ravenswood19@users.noreply.github.com>
2024-04-16 14:13:53 -0500

    Update child.h

--- docs/challenge/mark_and_luke_folder/child.h
+++ docs/challenge/mark_and_luke_folder/child.h
@@ -2,10 +2,6 @@
  * File: child.h
  * Author: Mark & Luke
  * Date: 4/11/24
- * Description: This file contains the implementation of the functions declared in the
- * child class Rectangle, which inherits from the parent class Shape. Since all the
- * virtual functions are already implemented in the header file, this file contains
- * empty implementations.
  */
 
 #include <iostream>
