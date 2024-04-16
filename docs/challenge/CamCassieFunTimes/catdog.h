15fc8d8be1a277697aeb07b72bcf85f6d0f7dc98
cwhittenhall <160174645+cwhittenhall@users.noreply.github.com>
2024-04-16 14:18:16 -0500

    Update catdog.h
    updated comments

--- docs/challenge/CamCassieFunTimes/catdog.h
+++ docs/challenge/CamCassieFunTimes/catdog.h
@@ -1,18 +1,19 @@
-// CatDog.h
+//Cassandra Whittenhall
+//CSCE 306
+//Source: code referenced from https://chat.openai.com/
 
 #ifndef CATDOG_H
 #define CATDOG_H
 
 #include "Animal.h"
 
+// Declaration of the Cat class, which inherits from the Animal class
 class Cat : public Animal {
 public:
-    void makeSound() const override;
+    void makeSound() const override; // Declaration of the makeSound function for Cat
 };
 
+// Declaration of the Dog class, which inherits from the Animal class
 class Dog : public Animal {
 public:
-    void makeSound() const override;
-};
-
-#endif // CATDOG_H
+    void makeSound() const override; // Declaration of the makeSound funct
