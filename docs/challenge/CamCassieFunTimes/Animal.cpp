b2199cb96de9633bdd9e254ec55f9531fc49563f
Cam Gugel <158515450+Camgoogs@users.noreply.github.com>
2024-04-16 14:14:36 -0500

    Update Animal.cpp

--- docs/challenge/CamCassieFunTimes/Animal.cpp
+++ docs/challenge/CamCassieFunTimes/Animal.cpp
@@ -1,5 +1,8 @@
+//Created by Camden Gugel
+//Some code implemented from chat.openai.com
+
 #include "Animal.h"
 
 void Animal::makeSound() const {
-    std::cout << "Roar!\n";
+    std::cout << "Roar!\n";    // Outputs a generic animal noise of "Roar!" to any unspecified animal
 }
