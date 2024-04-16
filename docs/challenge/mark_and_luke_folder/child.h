09c2d06bcf0665a96d02b094a32460207b8242e5
mfbavaro <159957534+mfbavaro@users.noreply.github.com>
2024-04-16 14:07:39 -0500

    Update child.h

--- docs/challenge/mark_and_luke_folder/child.h
+++ docs/challenge/mark_and_luke_folder/child.h
@@ -8,46 +8,8 @@
  * empty implementations.
  */
 
-#ifndef CHILD_H
-#define CHILD_H
-
-#include "parent.h"
 #include <iostream>
-
-// Child class representing a rectangle
-class Rectangle : public Shape {
-private:
-    double width; // Width of the rectangle
-    double height; // Height of the rectangle
-public:
-    /**
-     * @brief Constructor to initialize the width and height of the rectangle.
-     * @param w The width of the rectangle.
-     * @param h The height of the rectangle.
-     */
-    Rectangle(double w, double h) : width(w), height(h) {}
-
-    /**
-     * @brief Function to calculate the area of the rectangle.
-     * @return The area of the rectangle.
-     */
-    double area() const override { return width * height; }
-
-    /**
-     * @brief Function to calculate the perimeter of the rectangle.
-     * @return The perimeter of the rectangle.
-     */
-    double perimeter() const override { return 2 * (width + height); }
-
-    /**
-     * @brief Function to display information about the rectangle.
-     */
-    void display() const override {
-        std::cout << "Rectangle with width " << width << " and height " << height << std::endl;
-    }
-};
-
-#endif // CHILD_H
+#include "child.h"
 
 
 // Empty implementation for virtual functions
